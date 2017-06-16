b60e8ee81b59a85a6896c780122dbfc44edee55b
AkshathKotegar <itsakshath@gmail.com>
2017-06-16 10:30:27 +0530

    AddedProject

--- app/src/main/java/app/samplegit/thecreator/com/MainActivity.java
+++ app/src/main/java/app/samplegit/thecreator/com/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Super code HhHHhhHHhshshdhdhd
         String a="b";
+        a="dbdb";
     }
 }
